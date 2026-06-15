f43fab16440410a21d5fecbba9423bd610e0aaec
lucode-py <lucas.dugard@icloud.com>
2026-06-15 20:07:49 +0200

    modif final

--- fonction/minuteur.py
+++ fonction/minuteur.py
@@ -2,15 +2,17 @@ import time # Pour pouvoir faire des pauses
 
 # 1. On pose la question à l'utilisateur
 # input() attend que tu tapes quelque chose et que tu appuies sur Entrée
-choix = input("Veux-tu un timer de 15 ou 20 minutes ? (Tape le nombre) : ")
 
 # 2. On transforme ton texte en nombre (entier)
-chose_minutes = int(choix)
+chose_seconde = int(choix)
 
-def minuteur(minutes):
+def minuteur(seconde):
+
+    chose_seconde = int(seconde)
+
 
     # 3. On calcule le total de secondes
-    secondes = minutes * 60
+    secondes = seconde * 60
     # 4. La boucle qui compte
     while secondes > 0:
         print(f"Temps restant : {secondes} secondes")
@@ -19,4 +21,4 @@ def minuteur(minutes):
 
     print("BIIIIP ! C'est fini !")
 
-minuteur(chose_minutes)
+minuteur(chose_seconde)
